1dac4a8a6472aa16853fbdc1bb814d4e19299d21
Karan Vichare <34955037+karanvichare20@users.noreply.github.com>
2018-03-15 13:56:30 +0100

    Statis events on Schedular

--- SRH Sports Club Management/src/app/scheduler/scheduler.component.ts
+++ SRH Sports Club Management/src/app/scheduler/scheduler.component.ts
@@ -1,28 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
 import {ElementRef, ViewChild, ViewEncapsulation } from "@angular/core";
 import "dhtmlx-scheduler";
 import {} from "@types/dhtmlxscheduler";
-
-// @Component({
-//   selector: 'app-scheduler',
-//   templateUrl: './scheduler.component.html',
-//   styleUrls: ['./scheduler.component.css']
-// })
+import {EventService} from "../services/event.service";
+//import {Event} from "../models/Event";
+import { and } from '@angular/router/src/utils/collection';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
   selector: "scheduler",
   styleUrls: ['scheduler.component.css'],
-  templateUrl: 'scheduler.component.html'
+  templateUrl: 'scheduler.component.html',
+  providers: [EventService]
 })
 
 export class SchedulerComponent implements OnInit {
+
   @ViewChild("scheduler_here") schedulerContainer: ElementRef;
-  constructor() { }
+  constructor(private eventService: EventService){}
 
   ngOnInit(){
+      
+    scheduler.config.xml_date = "%Y-%m-%d %H:%i";
     scheduler.init(this.schedulerContainer.nativeElement, new Date());
-  }
-
+    this.eventService.get()
+        .then((data) => {
+            scheduler.parse(data, "json");
+        })
+}
+ 
 }
--- SRH Sports Club Management/src/app/services/event.service.ts
+++ SRH Sports Club Management/src/app/services/event.service.ts
@@ -1,2 +1,21 @@
 import {Injectable} from "@angular/core";
+//import {Http} from "@angular/http";
+//import {ExtractData, HandleError} from "./service-helper";
 import {Event} from "../models/event";
+//import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class EventService {
+ ///   private eventUrl = "api/events";
+///    constructor(private http: Http) {}
+    get(): Promise<any[]>{
+      //  return this.http.get(this.eventUrl)
+            //.toPromise()
+          //  .then(ExtractData)
+        //    .catch(HandleError);
+            return Promise.resolve([
+                {id: 1, start_date: "2018-03-22 08:00", end_date: "2018-03-22 13:00", text: "Football: Hall 1 & 2"},
+                {id: 2, start_date: "2018-03-20 09:00", end_date: "2018-03-20 13:00", text: "Badminton: Hall 1"},
+    ]);}
+   //insert update delete
+}
