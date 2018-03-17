a89f2db2b67269bf62181e9d6cfa2e34f3485b98
SandeshAthreyaBD <34373336+SandeshAthreyaBD@users.noreply.github.com>
2018-03-17 13:03:26 +0100

    chnged views
    put view components just inside scr component so that it will be easy for routing

--- SRH Sports Club Management/src/app/app.module.ts
+++ SRH Sports Club Management/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutes } from './app.routes';
+import { RouterModule } from '@angular/router';
+
 
 import { FormsModule } from '@angular/forms';
 import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './views/login/login.component';
-import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { environment } from '../environments/environment';
 import { AngularFireModule } from 'angularfire2';
@@ -17,11 +19,12 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 
 import { AuthService } from './services/auth.service';
-import { AuthGuard } from 'app/services/auth-guard.service';
+import { AuthGuard } from './services/auth-guard.service';
 import { SchedulerComponent } from './scheduler/scheduler.component';
-import { NavbarComponent } from './views/navbar/navbar.component';
-import { AboutComponent } from './views/about/about.component';
-import { ContactComponent } from './views/contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
 
 @NgModule({
   declarations: [
@@ -31,11 +34,12 @@ import { ContactComponent } from './views/contact/contact.component';
     SchedulerComponent,
     NavbarComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutes,
+    RouterModule,
     FormsModule,
     HttpModule,
     AngularFireModule.initializeApp(environment.firebase, 'angular-auth-firebase'),
--- SRH Sports Club Management/src/app/app.routes.ts
+++ SRH Sports Club Management/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 
-import { LoginComponent } from './views/login/login.component';
-import { DashboardComponent } from './views/dashboard/dashboard.component';
-import { NavbarComponent } from './views/navbar/navbar.component';
-import { AboutComponent } from './views/about/about.component';
-import { ContactComponent } from './views/contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
 
 import { AuthGuard } from './services/auth-guard.service';
 
