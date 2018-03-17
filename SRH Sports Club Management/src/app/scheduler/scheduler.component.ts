42b9de996e6899251f5ab73d4c52e398fee378cd
Biswarup <swarupkumar2@gmail.com>
2018-03-17 13:00:14 +0100

    Added CRUD operation from the scheduler to firebase

--- SRH Sports Club Management/src/app/scheduler/scheduler.component.ts
+++ SRH Sports Club Management/src/app/scheduler/scheduler.component.ts
@@ -3,7 +3,7 @@ import {ElementRef, ViewChild, ViewEncapsulation } from "@angular/core";
 import "dhtmlx-scheduler";
 import {} from "@types/dhtmlxscheduler";
 import {EventService} from "../services/event.service";
-//import {Event} from "../models/Event";
+import {Event} from "../models/Event";
 import { and } from '@angular/router/src/utils/collection';
 
 @Component({
@@ -17,16 +17,50 @@ import { and } from '@angular/router/src/utils/collection';
 export class SchedulerComponent implements OnInit {
 
   @ViewChild("scheduler_here") schedulerContainer: ElementRef;
+
   constructor(private eventService: EventService){}
 
   ngOnInit(){
-      
     scheduler.config.xml_date = "%Y-%m-%d %H:%i";
+
     scheduler.init(this.schedulerContainer.nativeElement, new Date());
+
+    scheduler.attachEvent("onEventAdded", (id, ev) => {
+        this.eventService.insert(this.serializeEvent(ev, true))
+            .then((response)=> {
+                if(response.id != id){
+                    scheduler.changeEventId(id, response.id);
+                }
+            })
+    });
+
+    scheduler.attachEvent("onEventChanged", (id, ev) => {
+        this.eventService.update(this.serializeEvent(ev));
+    });
+
+    scheduler.attachEvent("onEventDeleted", (id) => {
+        this.eventService.remove(id);
+    });
+
     this.eventService.get()
         .then((data) => {
             scheduler.parse(data, "json");
         })
+}
+
+private serializeEvent(data: any, insert: boolean = false): Event {
+  var result = {};
+
+  for(let i in data){
+      if(i.charAt(0) == "$" || i.charAt(0) == "_") continue;
+      if(insert && i == "id") continue;
+      if(data[i] instanceof Date){
+          result[i] = scheduler.templates.xml_format(data[i]);
+      } else {
+          result[i] = data[i];
+      }
+  }
+  return result as Event;
 }
  
 }
--- SRH Sports Club Management/src/app/services/event.service.ts
+++ SRH Sports Club Management/src/app/services/event.service.ts
@@ -1,21 +1,86 @@
 import {Injectable} from "@angular/core";
-//import {Http} from "@angular/http";
-//import {ExtractData, HandleError} from "./service-helper";
 import {Event} from "../models/event";
-//import 'rxjs/add/operator/toPromise';
+import {Http} from "@angular/http";
+import {ExtractData, HandleError} from "./service-helper";
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { getLocaleDateTimeFormat } from '@angular/common';
+
+
+import 'rxjs/add/operator/toPromise';
+import { Observable } from "rxjs/Observable";
+import { DataSnapshot } from "@firebase/database";
 
 @Injectable()
 export class EventService {
- ///   private eventUrl = "api/events";
-///    constructor(private http: Http) {}
-    get(): Promise<any[]>{
-      //  return this.http.get(this.eventUrl)
-            //.toPromise()
-          //  .then(ExtractData)
-        //    .catch(HandleError);
-            return Promise.resolve([
-                {id: 1, start_date: "2018-03-22 08:00", end_date: "2018-03-22 13:00", text: "Football: Hall 1 & 2"},
-                {id: 2, start_date: "2018-03-20 09:00", end_date: "2018-03-20 13:00", text: "Badminton: Hall 1"},
-    ]);}
-   //insert update delete
+ // private eventUrl = "api/event";
+
+ private eventUrl = "https://srh-authentication.firebaseio.com/event.json";
+
+  items: Observable<any[]>;
+  child: Observable<any[]>;
+
+	constructor(public db: AngularFireDatabase, private http: Http) {}
+
+
+	get(): Promise<Event[]>{
+
+		console.log(this.http.get(this.eventUrl).toPromise().catch(HandleError));
+		return this.http.get(this.eventUrl)
+			.toPromise().catch(HandleError);
+	}
+
+/*	get(): Promise<Event[]>{
+
+		this.items = this.db.list('event').snapshotChanges();
+
+		this.items.subscribe(data => {
+
+			console.log(data);
+
+			data.forEach(snapshot => {
+				
+				var temp = {
+					key: "",
+					value: ""
+				};
+				temp.key = snapshot.key;
+				temp.value = snapshot.payload.val();
+				console.log(temp.value);
+
+			});
+		});
+		
+		return this.promise.then(ExtractData).catch(HandleError);
+	}*/
+  
+/*	insert(event: Event): Promise<Event> {
+		return this.http.post(this.eventUrl, JSON.stringify(event))
+			.toPromise()
+			.then(ExtractData)
+			.catch(HandleError);
+	}*/
+
+  insert(event: Event): any {
+
+	let ndate = new Date();
+	
+	console.log(ndate.getTime());
+
+	return this.db.list('event').set(ndate.getTime().toString(), JSON.stringify(event));
+	//	return this.db.list('event').push(JSON.stringify(event));
+  }
+
+	update(event: Event): Promise<void> {
+		return this.http.put(`${this.eventUrl}/${event.id}`, JSON.stringify(event))
+			.toPromise()
+			.then(ExtractData)
+			.catch(HandleError);
+  }
+
+	remove(id: number): Promise<void> {
+		return this.http.delete(`${this.eventUrl}/${id}`)
+			.toPromise()
+			.then(ExtractData)
+			.catch(HandleError);
+	}
 }
